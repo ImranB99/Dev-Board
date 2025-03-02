3964d67908b3bb2bafe34f5e104e28e09544d471
ImranB99 <imranbiswas9299@gmail.com>
2025-03-02 20:37:33 +0600

    added javascript

---
+++ js/dev.js
@@ -0,0 +1,85 @@
+// index.html to blog.html
+
+document.getElementById('discover')
+.addEventListener('click', function(event){
+    event.preventDefault();
+    // console.log("go")
+    window.location.href="blog.html"
+})
+
+
+
+// Completed Button
+let counter = 6;
+let counter2 = 23;
+const counterElement = document.getElementById('counter');
+const counter2Element = document.getElementById('counter2');
+
+
+document.getElementById('button1')
+.addEventListener('click', function(event) {
+  alert('Board updated successfully');
+  counter--;
+  counter2++;
+  counterElement.innerText = counter;
+  counter2Element.innerText = counter2;
+  event.target.disabled = true;
+
+});
+
+document.getElementById('button2')
+.addEventListener('click', function(event) {
+  alert('Board updated successfully');
+  counter--;
+  counter2++;
+  counterElement.innerText = counter;
+  counter2Element.innerText = counter2;
+  event.target.disabled = true;
+});
+
+document.getElementById('button3')
+.addEventListener('click', function(event) {
+  alert('Board updated successfully');
+  counter--;
+  counter2++;
+  counterElement.innerText = counter;
+  counter2Element.innerText = counter2;
+  event.target.disabled = true;
+});
+
+document.getElementById('button4')
+.addEventListener('click', function(event) {
+  alert('Board updated successfully');
+  counter--;
+  counter2++;
+  counterElement.innerText = counter;
+  counter2Element.innerText = counter2;
+  event.target.disabled = true;
+});
+
+document.getElementById('button5')
+.addEventListener('click', function(event) {
+  alert('Board updated successfully');
+  counter--;
+  counter2++;
+  counterElement.innerText = counter;
+  counter2Element.innerText = counter2;
+  event.target.disabled = true;
+});
+
+document.getElementById('button6')
+.addEventListener('click', function(event) {
+  alert('Board updated successfully');
+  counter--;
+  counter2++;
+  counterElement.innerText = counter;
+  counter2Element.innerText = counter2;
+  event.target.disabled = true;
+});
+
+
+
+
+   
+
+          
